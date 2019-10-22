59b8c117ebadca63da2da434fa8aa211a3c4a484
Frank <frankli1009@gmail.com>
2019-10-22 08:49:38 +0100

    Add json parameters

---
+++ EvalStr/Model/CalculatorParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvalStr.Model
+{
+    public class CalculatorResponse
+    {
+        public System.Net.HttpStatusCode StatusCode { get; set; }
+        public string Result { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+
+    public class CalculatorRequest
+    {
+        public string Expression { get; set; }
+    }
+}
