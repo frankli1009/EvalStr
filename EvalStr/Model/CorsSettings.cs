43ac071225770c7b1f66c22319145cccc173cb3c
Frank <frankli1009@gmail.com>
2019-10-23 08:59:14 +0100

    Add json support models

---
+++ EvalStr/Model/CorsSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvalStr.Model
+{
+    public class CorsSettings
+    {
+        public IEnumerable<string> Hosts { get; set; }
+    }
+}
